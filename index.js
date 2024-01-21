e5e488cd1714874496a686aa510593be8036ff87
sahanilxm <laxmisahani240@gmail.com>
2024-01-21 20:23:44 +0530

    Some styling done.

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
             return response.json().then( profile => {
                 totalRepositories = parseInt(profile.public_repos, 10);
-                console.log(totalRepositories);
                 displayProfile(profile);
                 fetchRepositories(apiUrl, currentPage, perPage);
                 });
@@ -39,9 +38,11 @@ document.addEventListener('DOMContentLoaded', function () {
         function displayProfile(profile){
             const container = document.getElementById('profile-container');
             container.innerHTML = '';
+            container.classList.add('secondary-text');
 
             const userName = document.createElement('h3');
-            userName.innerHTML = profile?.name;
+            userName.classList.add('fw-bold', 'text-decoration-underline', 'primary-text')
+            userName.innerHTML = profile?.name || profile?.login;
             container.append(userName);
 
             const userBio = document.createElement('p');
@@ -79,13 +80,31 @@ document.addEventListener('DOMContentLoaded', function () {
             if(repositories.length === 0){
                 container.innerHTML = '<p>No repositories found.</p>';
             }else{
-                const list = document.createElement('ul');
                 repositories?.forEach(repo => {
-                const listItem = document.createElement('li');
-                listItem.innerHTML = `<strong>${repo.name}</strong>: ${repo.description || 'No description'}`;
-                list.appendChild(listItem);
+                    const repository = document.createElement('div');
+                    repository.classList.add('w-25', 'm-2', 'p-2', 'repo');
+                    const repositoryName = document.createElement('strong');
+                    repositoryName.classList.add('primary-text', 'text-decoration-underline');
+                    repositoryName.innerHTML = `${repo.name}`;
+                    const repositoryDescription = document.createElement('p');
+                    repositoryDescription.classList.add('helper-text', 'mt-4');
+                    repositoryDescription.innerHTML = `${repo.description || 'No description'}`;
+
+                    const repositoryTopics = document.createElement('div');
+                    repositoryTopics.classList.add('d-flex', 'flex-wrap');
+                    const repositoryTopicsList = repo.topics;
+                    repositoryTopicsList?.map((topic) => {
+                        const repositoryTopic = document.createElement('button');
+                        repositoryTopic.classList.add('m-1', 'secondary-btn');
+                        repositoryTopic.innerHTML = topic;
+                        repositoryTopics.appendChild(repositoryTopic);
+                    })
+
+                    repository.appendChild(repositoryName);
+                    repository.appendChild(repositoryDescription);
+                    repository.appendChild(repositoryTopics);
+                    container.appendChild(repository);
                 });
-                container.appendChild(list);
             }
         }
 
@@ -97,6 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const prevButton = document.createElement('button');
             prevButton.innerText = 'Previous';
+            prevButton.classList.add('my-4', 'mx-2', 'primary-btn');
             prevButton.addEventListener('click', function () {
                 if (currentPage > 1) {
                     currentPage--;
@@ -107,10 +127,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const currentPageElement = document.createElement('span');
             currentPageElement.innerText = `Page ${currentPage}`;
+            currentPageElement.classList.add('my-4', 'mx-2', 'secondary-btn');
             paginationContainer.appendChild(currentPageElement);
 
             const nextButton = document.createElement('button');
             nextButton.innerText = 'Next';
+            nextButton.classList.add('my-4', 'mx-2', 'primary-btn');
             nextButton.addEventListener('click', function () {
                 console.log('Next Button Clicked');
                 if (currentPage < totalPages) {
@@ -133,7 +155,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 currentPage = 1; 
                 fetchRepositories(apiUrl, currentPage, perPage);
             });
-
+            perPageSelect.classList.add('my-4', 'mx-2', 'secondary-btn');
             paginationContainer.appendChild(perPageSelect);
         }
     }
